1b24ecfd37b98b689715218596b34ec2345773f7
charlie  lee <ctlee9096@gmail.com>
2018-12-16 01:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function driversWithRevenueOver (collection, revenue) {
   return collection.filter(function(object) {return object.revenue >= revenue;
   });
 }
+
+function driverNamesWithRevenueOver(collection, number) {
+  const newArr = [];
+  return collection.filter(function(object){
+    if(object.revenue>=number) {
+      newArr.push(object.name)
+    }
+  })
+  return newArr;
+}
